02dba3ed62f553798f4d618552a99ae38d58670b
axelToussenel <axel.toussenel@gmail.com>
2024-04-18 13:52:56 +0200

    init project

---
+++ pages/components/dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => (
+  <p>
+    dashboard
+  </p>
+)
+
+export default Dashboard
---
+++ server.py
@@ -0,0 +1,8 @@
+
+#Exemple
+"""app.route('/')
+def get_data():
+    return 'Hello World'
+
+def ML():
+    return 'Machine Learning'"""
